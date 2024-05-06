ab3e9e2938767176366bf57eac6b79b4acc7afe4
dattm27 <dattran2003.ttn@gmail.com>
2024-05-06 22:30:35 +0700

    add bt tren lop 18.3 info client-server, text client-server

--- exercise/18.3.2024/text_client.c
+++ exercise/18.3.2024/text_client.c
@@ -1,3 +1,10 @@
+/*
+Ứng dụng client đọc nội dung file văn bản và gửi sang server.
+Ứng dụng server nhận dữ liệu từ client, in ra màn hình số lần xuất hiện xâu ký tự (server không cần tạo file để chứa nội dung nhận được): “0123456789”
+Chú ý cần xử lý trường hợp khi xâu “0123456789” nằm giữa 2 lần truyền.
+Ví dụ nội dung file văn bản:
+SOICTSOICT0123456789012345678901234567890123456789SOICTSOICTSOICT01234567890123456789012345678901234567890123456789012345678901234567890123456789SOICTSOICT
+*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/types.h>
@@ -44,6 +51,9 @@ int main () {
         send(client, buf, ret,0);
     }
     printf("Sent successfully!");
+    
+    
+    
     fclose(f);
 
     close(client);
